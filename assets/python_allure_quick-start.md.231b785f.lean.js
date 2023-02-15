009a2273a6e6d309408b362dfb96b51d0ea3589d
lzhgostudy <lzhgostudy@users.noreply.github.com>
2023-02-15 03:29:22 +0000

    deploy: 94db8be1c31788326fabaf0793efcee80f58d90e

---
+++ assets/python_allure_quick-start.md.231b785f.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as e,a as l}from"./app.a79a9890.js";const h=JSON.parse('{"title":"Get Started","description":"","frontmatter":{},"headers":[],"relativePath":"python/allure/quick-start.md"}'),t={name:"python/allure/quick-start.md"},n=l("",10),o=[n];function r(p,c,i,d,u,y){return a(),e("div",null,o)}const _=s(t,[["render",r]]);export{h as __pageData,_ as default};
